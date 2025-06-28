f20a978dfaebb24c52526a01391c8f2503fcb3f4
Paul Pietzko <paul.pietzko@icloud.com>
2025-06-28 22:33:23 +0200

    feat: add translations for footer and navbar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,33 +1,38 @@
 import { Wrench } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 export default function Footer() {
+  const t = useTranslations("Footer");
+
   return (
     <footer className="border-t border-gray-100 mt-16">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
-            <div className="flex items-center space-x-2">
-              <Wrench className="h-5 w-5 text-blue-600" />
-              <span className="text-sm text-gray-600">
-                © {new Date().getFullYear()} <a href="https://www.paulpietzko.com">Paul Pietzko</a>. Open source and free to use.
-              </span>
-            </div>
-            <div className="flex items-center space-x-6">
-              <Link
-                href="https://github.com/paulpietzko/opentools/blob/main/CONTRIBUTING.md"
-                className="text-sm text-gray-600 hover:text-gray-900"
-              >
-                Contributing Guide
-              </Link>
-              <Link
-                href="https://github.com/paulpietzko/opentools/"
-                className="text-sm text-gray-600 hover:text-gray-900"
-              >
-                GitHub
-              </Link>
-            </div>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+          <div className="flex items-center space-x-2">
+            <Wrench className="h-5 w-5 text-blue-600" />
+            <span className="text-sm text-gray-600">
+              © {new Date().getFullYear()}{" "}
+              <a href="https://www.paulpietzko.com">Paul Pietzko</a>.{" "}
+              {t("allRightsReserved")}
+            </span>
+          </div>
+          <div className="flex items-center space-x-6">
+            <Link
+              href="https://github.com/paulpietzko/opentools/blob/main/CONTRIBUTING.md"
+              className="text-sm text-gray-600 hover:text-gray-900"
+            >
+              {t("contributingGuide")}
+            </Link>
+            <Link
+              href="https://github.com/paulpietzko/opentools/"
+              className="text-sm text-gray-600 hover:text-gray-900"
+            >
+              GitHub
+            </Link>
           </div>
         </div>
-      </footer>
-  )
+      </div>
+    </footer>
+  );
 }
